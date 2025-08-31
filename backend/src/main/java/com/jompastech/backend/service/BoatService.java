dbcd11251c32ef1342f6f316ee75c51afde7644e
jpfernandes1 <jp_fernandes1@live.com>
2025-08-30 22:29:53 -0300

    feat: Add findAllBoat to BoatService

--- backend/src/main/java/com/jompastech/backend/service/BoatService.java
+++ backend/src/main/java/com/jompastech/backend/service/BoatService.java
@@ -1,29 +1,34 @@
 package com.jompastech.backend.service;
 
+import com.jompastech.backend.mapper.BoatMapper;
+import com.jompastech.backend.model.dto.BoatResponseDTO;
+import com.jompastech.backend.model.dto.UserResponseDTO;
 import com.jompastech.backend.model.entity.Boat;
 import com.jompastech.backend.repository.BoatRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class BoatService {
 
     private final BoatRepository boatRepository;
+    private final BoatMapper boatMapper;
 
-    public BoatService(BoatRepository boatRepository) {
+    public BoatService(BoatRepository boatRepository, BoatMapper boatMapper) {
         this.boatRepository = boatRepository;
+        this.boatMapper = boatMapper;
     }
 
-    // CRUD básico
+    // CRUD
     public Boat save(Boat boat) {
         return boatRepository.save(boat);
     }
 
     public Boat findById(Long id) {
         return boatRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Boat não encontrado"));
+                .orElseThrow(() -> new RuntimeException("Boat not found!"));
     }
 
     public List<Boat> findAll() {
@@ -34,12 +39,19 @@ public class BoatService {
         boatRepository.deleteById(id);
     }
 
-    // Exemplos de métodos adicionais de negócio
+    // Aditional business methods
     public List<Boat> findByType(String type) {
         return boatRepository.findByType(type);
     }
 
     public List<Boat> findByOwnerId(Long ownerId) {
         return boatRepository.findByOwnerId(ownerId);
+    }
+
+    public List<BoatResponseDTO> findAllBoats() {
+        return boatRepository.findAll()
+                .stream()
+                .map(boatMapper::toResponseDTO)
+                .collect(Collectors.toList());
     }
 }
