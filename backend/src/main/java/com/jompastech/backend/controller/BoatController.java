c79a99fdcbb767979c7001af72a39ca1405e082b
jpfernandes1 <jp_fernandes1@live.com>
2025-08-30 22:24:38 -0300

    feat(controller): Add getAllBoats on BoatController;

--- backend/src/main/java/com/jompastech/backend/controller/BoatController.java
+++ backend/src/main/java/com/jompastech/backend/controller/BoatController.java
@@ -3,12 +3,15 @@ package com.jompastech.backend.controller;
 import com.jompastech.backend.mapper.BoatMapper;
 import com.jompastech.backend.model.dto.BoatRequestDTO;
 import com.jompastech.backend.model.dto.BoatResponseDTO;
+import com.jompastech.backend.model.dto.UserResponseDTO;
 import com.jompastech.backend.model.entity.Boat;
 import com.jompastech.backend.service.BoatService;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/boats")
@@ -29,6 +32,12 @@ public class BoatController {
     public ResponseEntity<BoatResponseDTO> getBoat(@PathVariable Long id) {
         Boat boat = boatService.findById(id);
         return ResponseEntity.ok(boatMapper.toResponseDTO(boat));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<BoatResponseDTO>> getAllBoats() {
+
+        return ResponseEntity.ok(boatService.findAllBoats());
     }
 
 }
