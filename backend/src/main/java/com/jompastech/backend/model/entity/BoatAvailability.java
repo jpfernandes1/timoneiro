0b16426f9c0e783c7f8b2d02699fff38c8ff9bd5
jpfernandes1 <jp_fernandes1@live.com>
2025-10-28 18:59:10 -0300

    feat: add date validation and utility methods to BoatAvailability entity

--- backend/src/main/java/com/jompastech/backend/model/entity/BoatAvailability.java
+++ backend/src/main/java/com/jompastech/backend/model/entity/BoatAvailability.java
@@ -3,6 +3,7 @@ package com.jompastech.backend.model.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 
 @Entity
@@ -15,14 +16,34 @@ public class BoatAvailability {
     @Column(name = "availability_id")
     private long id;
 
-    @ManyToOne
-    @JoinColumn(name = "boat_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "boat_id", nullable = false)
     private Boat boat;
 
-    @Column(name = "start_date")
+    @Column(name = "start_date", nullable = false)
     private LocalDateTime startDate;
 
-    @Column(name = "end_date")
+    @Column(name = "end_date", nullable = false)
     private LocalDateTime endDate;
+
+    // Validations
+    public BoatAvailability(Boat boat, LocalDateTime startDate, LocalDateTime endDate) {
+        if (startDate.isAfter(endDate)) {
+            throw new IllegalArgumentException("Start date must be before end date");
+        }
+        this.boat = boat;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    // Util method to extract day os week
+    public DayOfWeek getDayOfWeek() {
+        return startDate.getDayOfWeek();
+    }
+
+    // Method to check if a period is on this availability
+    public boolean coversPeriod(LocalDateTime checkStart, LocalDateTime checkEnd) {
+        return !checkStart.isBefore(startDate) && !checkEnd.isAfter(endDate);
+    }
 
 }
