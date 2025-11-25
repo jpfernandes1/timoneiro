d3da6e834b92eff1dfd27d1417d5b00ba7ca6b26
jpfernandes1 <jp_fernandes1@live.com>
2025-11-25 11:46:58 -0300

    refactor(BoatDTOS): insert AllArgs constructor

--- backend/src/main/java/com/jompastech/backend/model/dto/BoatRequestDTO.java
+++ backend/src/main/java/com/jompastech/backend/model/dto/BoatRequestDTO.java
@@ -2,11 +2,13 @@ package com.jompastech.backend.model.dto;
 
 import com.jompastech.backend.model.entity.Address;
 import com.jompastech.backend.model.entity.User;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
 public class BoatRequestDTO {
     private String name;
     private String description;
--- backend/src/main/java/com/jompastech/backend/model/dto/BoatResponseDTO.java
+++ backend/src/main/java/com/jompastech/backend/model/dto/BoatResponseDTO.java
@@ -2,10 +2,12 @@ package com.jompastech.backend.model.dto;
 
 import com.jompastech.backend.model.entity.Address;
 import com.jompastech.backend.model.entity.User;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
 public class BoatResponseDTO {
     private Long id;
     private String name;
