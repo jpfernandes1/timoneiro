1921945055fef5735692cc07e3b17c49592f7581
jpfernandes1 <jp_fernandes1@live.com>
2025-11-29 15:01:39 -0300

    refactor: add @AllArgsConstructor to User DTOs

--- backend/src/main/java/com/jompastech/backend/model/dto/UserRequestDTO.java
+++ backend/src/main/java/com/jompastech/backend/model/dto/UserRequestDTO.java
@@ -4,10 +4,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.hibernate.validator.constraints.br.CPF;
 
 @Data
+@AllArgsConstructor
 public class UserRequestDTO {
 
     @NotBlank
